96ababa8646e449a1a290ce656457d08b3bc9c3d
kofki <69553679+kofki@users.noreply.github.com>
2024-12-25 21:52:01 -0500

    Added a basic version of Push Notifications for testing

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,73 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { useState, useEffect } from 'react';
 import { MyTabs } from './navigation/index';
+import * as Notifications from 'expo-notifications';
+import { Platform } from 'react-native';
+
+Notifications.setNotificationHandler({
+  handleNotification: async() =>({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 
 export default function App() {
+  useEffect(() =>{
+    const registerForPushNotifications = async() =>{
+      if (Platform.OS === 'android'){
+        await Notifications.setNotificationChannelAsync('default', {
+          name: 'default',
+          importance: Notifications.AndroidImportance.MAX,
+        });
+      }
+      const { status } = await Notifications.requestPermissionsAsync();
+      if (status !== 'granted'){
+        alert('No notification permissions')
+      }
+
+      const token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log('Expo Push Token: ', token);
+    };
+
+    registerForPushNotifications();
+
+
+    const scheduleNotifications = async() => {
+      await Notifications.cancelAllScheduledNotificationsAsync();
+      
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "test 1",
+          body: "test 1 message",
+          sound: true,
+        },
+        trigger: {
+          seconds: 1000,
+        }
+      });
+    };
+
+    scheduleNotifications();
+    
+    const notificationListener = Notifications.addNotificationReceivedListener(
+      notification => {
+        console.log("notification received: ", notification);
+      }
+    );
+
+    const responseListener = Notifications.addNotificationResponseReceivedListener(
+      response => {
+        console.log("Notification interaction: ", response);
+      }
+    );
+
+    return () => {
+      notificationListener.remove();
+      responseListener.remove();
+    };
+  }, []);
+
   return (
     <MyTabs/>
   );  
